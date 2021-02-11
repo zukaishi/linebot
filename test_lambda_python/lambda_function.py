b8893d502a1ecd97d66eda41b6d13d9fa8fafc42
zukaishi <ishizuka215@gmail.com>
2021-02-12 06:52:13 +0900

    update

--- test_lambda_python/lambda_function.py
+++ test_lambda_python/lambda_function.py
@@ -1,8 +1,9 @@
-import os, sys, json
+import os, sys, json, datetime
 import requests
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 from linebot.exceptions import (LineBotApiError, InvalidSignatureError)
+from pytz import timezone
 
 line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
 handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
@@ -15,20 +16,59 @@ def lambda_handler(event, context):
     ok_json = os.environ["ok_json"]
     error_json = os.environ["error_json"]
 
-    # weather
-    url = "https://community-open-weather-map.p.rapidapi.com/weather"
-    querystring = {"q":"Tokyo,jp","units":"metric","lang":"ja"}
-    headers = {
-        'x-rapidapi-key': os.environ["X_RAPIDAPI_KEY"],
-        'x-rapidapi-host': os.environ["X_RAPIDAPI_HOST"]
-    }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    print(response.text)
-    
     @handler.add(MessageEvent, message=TextMessage)
     def message(line_event):
-        text = line_event.message.text
+        #text = line_event.message.text
+        text = getWeather()
         line_bot_api.reply_message(line_event.reply_token, TextSendMessage(text=text)) 
+
+    def getWeather():
+        url = "https://community-open-weather-map.p.rapidapi.com/forecast"
+        querystring = {"q":"Kyoto,jp","units":"metric","lang":"ja"}
+        headers = {
+            'x-rapidapi-key': os.environ["X_RAPIDAPI_KEY"],
+            'x-rapidapi-host': os.environ["X_RAPIDAPI_HOST"]
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        print(response.text)
+        forecastData = json.loads(response.text)
+        
+        if not ('list' in forecastData):
+            print('error')
+            return
+        
+        # 通知内容作成
+        words = '【今日の天気】\n'
+        beforeDate = ''
+        print(forecastData['list'][0])
+        for item in forecastData['list']:
+            forecastDatetime = timezone('Asia/Tokyo').localize(datetime.datetime.fromtimestamp(item['dt']))
+
+            # 本日分のみ通知対象とする
+            if beforeDate != '' and beforeDate != forecastDatetime.strftime('%Y-%m-%d'):
+                break
+            else:
+                beforeDate = forecastDatetime.strftime('%Y-%m-%d')
+
+            weatherDescription = item['weather'][0]['description']
+            emoji = ''
+            # 絵文字の分岐は適当
+            if '曇' in weatherDescription:
+                emoji = '\uDBC0\uDCAC'
+            elif '雪' in weatherDescription:
+                emoji = '\uDBC0\uDCAB'
+            elif '雨' in weatherDescription:
+                emoji = '\uDBC0\uDCAA'
+            elif '晴' in weatherDescription:
+                emoji = '\uDBC0\uDCA9'
+    
+            temperature = item['main']['temp']
+            rainfall = 0
+            if 'rain' in item and '3h' in item['rain']:
+                rainfall = item['rain']['3h']
+            words += '\n{0}\n天気:{1} {2}\n気温(℃):{3}\n雨量(mm):{4}\n'.format(forecastDatetime.strftime('%Y-%m-%d %H:%M'), emoji, weatherDescription, temperature, rainfall)
+        
+        return words
 
     try:
         handler.handle(body, signature)
