44a3f4f7db8bffea8cdff8c32d3fab7d080f48af
zukaishi <ishizuka215@gmail.com>
2021-02-13 07:15:56 +0900

    update

--- test_lambda_python/lambda_function.py
+++ test_lambda_python/lambda_function.py
@@ -1,26 +1,17 @@
-import os, sys, json, datetime,time 
+import os, sys, json,time,datetime
 import requests
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 from linebot.exceptions import (LineBotApiError, InvalidSignatureError)
 from pytz import timezone
+from datetime import timedelta,timezone
 
 line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
 handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
 
 def lambda_handler(event, context):
-    print(event)
-    signature = event["headers"]["x-line-signature"]
-    body = event["body"]
-
     ok_json = os.environ["ok_json"]
     error_json = os.environ["error_json"]
-
-    @handler.add(MessageEvent, message=TextMessage)
-    def message(line_event):
-        #text = line_event.message.text
-        text = getWeather()
-        line_bot_api.reply_message(line_event.reply_token, TextSendMessage(text=text)) 
 
     def getWeather():
         url = "https://community-open-weather-map.p.rapidapi.com/forecast"
@@ -40,10 +31,8 @@ def lambda_handler(event, context):
         
         # 通知内容作成
         words = '【今日の天気】\n'
-        # JST = timezone(timedelta(hours=+9), 'JST')
+        JST = timezone(timedelta(hours=+9), 'JST')
         for item in forecastData['list']:
-            forecastDatetime = timezone('Asia/Tokyo').localize(datetime.datetime.fromtimestamp(item['dt']))
-
             if item['dt'] < time.time() or item['dt'] > (time.time()  + 86400):
                 break
 
@@ -63,9 +52,22 @@ def lambda_handler(event, context):
             rainfall = 0
             if 'rain' in item and '3h' in item['rain']:
                 rainfall = item['rain']['3h']
-            words += '\n{0}\n天気:{1} {2}\n気温(℃):{3}\n雨量(mm):{4}\n'.format(item['dt'], emoji, weatherDescription, temperature, rainfall)
+            words += '\n{0}\n天気:{1} {2}\n気温(℃):{3}\n雨量(mm):{4}\n'.format(datetime.datetime.fromtimestamp(item['dt'], JST), emoji, weatherDescription, temperature, rainfall)
         
         return words
+
+    @handler.add(MessageEvent, message=TextMessage)
+    def message(line_event):
+        text = getWeather()
+        line_bot_api.reply_message(line_event.reply_token, TextSendMessage(text=text)) 
+
+    print(event)
+    if "headers" in event:
+        signature = event["headers"]["x-line-signature"]
+        body = event["body"]
+    else:
+        text = getWeather()
+        return ok_json
 
     try:
         handler.handle(body, signature)
