20e6627f0f3dacbd417d87a466cc947cdd4105f5
zukaishi <ishizuka215@gmail.com>
2021-08-12 06:51:08 +0900

    update

--- todolist/src/cmd/todolist.go
+++ todolist/src/cmd/todolist.go
@@ -6,19 +6,55 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
+
+type todolist struct {
+	mid          string `json:"mid"`
+	unixtime     int    `json:"unixtime"`
+	todo_name    string `json:"todo_name"`
+	comment      string `json:"comment"`
+	start        int    `json:"start"`
+	start_alarm  int    `json:"start_alarm"`
+	end          int    `json:"end"`
+	end_alarm    int    `json:"end_alarm"`
+	routine_flag int    `json:"routine_flag"`
+	delete_flag  int    `json:"delete_flag"`
+}
 
 var db = dynamodb.New(session.New(), aws.NewConfig().WithRegion("ap-northeast-1"))
 
 func main() {
 	hoge()
-	fuga()
 }
 
 func hoge() {
 	fmt.Println("Hello hoge!")
 }
 
-func fuga() {
-	fmt.Println("Hello fuga!")
+func getItem(isbn string) (*todolist, error) {
+	input := &dynamodb.GetItemInput{
+		TableName: aws.String("todolist"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"mid": {
+				S: aws.String(isbn),
+			},
+		},
+	}
+
+	result, err := db.GetItem(input)
+	if err != nil {
+		return nil, err
+	}
+	if result.Item == nil {
+		return nil, nil
+	}
+
+	bk := new(todolist)
+	err = dynamodbattribute.UnmarshalMap(result.Item, bk)
+	if err != nil {
+		return nil, err
+	}
+
+	return bk, nil
 }
