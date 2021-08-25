6ef15b9941abcd5bd02c8d8245d8618cbed501d3
zukaishi <ishizuka215@gmail.com>
2021-08-26 07:01:12 +0900

    update

--- todolist/src/php/index.php
+++ todolist/src/php/index.php
@@ -1,12 +1,35 @@
 <?php
-require("index2.php");
+require 'aws/aws-autoloader.php';
+$array_ini_file = parse_ini_file('credentials.ini', true);
 
-$array_test = array(
-  '1' => 'apple',
-  '2' => 'orange', 
-  '3' => 'banana', 
-  '4' => 'melon', 
-  '5' => 'grape',
-  '6' => 'pineapple',
-);
-echo json_encode($array_test);
+// S3Clientインスタンスの作成
+$s3client = Aws\S3\S3Client::factory([
+    'credentials' => [
+        'key'       => $array_ini_file['aws_access_key_id'],
+        'secret'    => $array_ini_file['aws_secret_access_key'],
+    ],
+    'region' => 'ap-northeast-1',
+    'version' => 'latest',
+]);
+
+// ローカルにファイル作成
+$file    = date('YmdHis') . '.txt';
+$content = <<< EOF
+hoge
+ふが
+EOF;
+file_put_contents($file, $content);
+
+// S3にアップロード
+$result = $s3client->putObject([
+    'ACL'           => 'public-read',   // ACLを指定する場合、ブロックパブリックアクセスはすべてオフにする
+    'Bucket'        => $array_ini_file['aws_bucket_name'],
+    'Key'           => 'test.txt',
+    'SourceFile'    => $file,
+    'ContentType'   => mime_content_type($file),
+]);
+
+// 結果を表示
+echo('<pre>');
+var_dump($result);
+echo('</pre>');
