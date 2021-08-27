35de51d77ba672b55f217ddb6abff1cb7037082c
zukaishi <ishizuka215@gmail.com>
2021-08-28 07:01:56 +0900

    update

--- todolist/src/php/index.php
+++ todolist/src/php/index.php
@@ -3,6 +3,7 @@ require 'aws/aws-autoloader.php';
 $array_ini_file = parse_ini_file('credentials.ini', true);
 
 // S3Clientインスタンスの作成
+/*
 $s3client = Aws\S3\S3Client::factory([
     'credentials' => [
         'key'       => $array_ini_file['aws_access_key_id'],
@@ -28,6 +29,28 @@ $result = $s3client->putObject([
     'SourceFile'    => $file,
     'ContentType'   => mime_content_type($file),
 ]);
+
+
+*/
+
+$dynamodbclient = Aws\DynamoDb\DynamoDbClient::factory([
+  'credentials' => [
+    'key'       => $array_ini_file['aws_access_key_id'],
+    'secret'    => $array_ini_file['aws_secret_access_key'],
+  ],
+  'region' => 'ap-northeast-1',
+  'version' => 'latest',
+]);
+
+$args = array(
+  'TableName' => 'table_name',
+  'Key' => array(
+      'field1' => array('N' => '1201'),
+      'field2' => array('S' => 1),
+  ),
+  'ConsistentRead' => true,
+);
+$result = $dynamodbclient->getItem($args);
 
 // 結果を表示
 echo('<pre>');
